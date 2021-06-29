d0150158b81b53d5d5c0b314ab99446c0d66bf63
rodzglory <rodz.glory@hotmail.com>
2021-06-29 00:31:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from random import choice
 #Constants
 BOT = 'Achilles'
 VERSION = '0.1.0'
-    #paths
+    #Paths
 BOTDIR = os.path.dirname(__file__)
 INFO = os.path.join(BOTDIR, 'infos') 
 TOKEN = open(f'{INFO}/botinfo.txt').readlines()[0]
@@ -28,13 +28,13 @@ ACTIVITIES = os.path.join(INFO, 'activities.txt')
 AUDIO = os.path.join(BOTDIR, 'audio')
 SAMPLE = os.path.join(AUDIO, 'sample')
 
-client = commands.Bot(command_prefix=PREFIXES) #calls the bot object, the most important thing to make the bot work
-scheduler = AsyncIOScheduler() #scheduler for automated tasks
+client = commands.Bot(command_prefix=PREFIXES) #Calls the bot object, the most important thing to make the bot work.
+scheduler = AsyncIOScheduler() #Scheduler for automated tasks.
 
 def status(message: str = 'Probably forgot the status...') -> print:
     """Changes the console status
     """
-    print(f'{message} {localtime()[3]}:{localtime()[4]}:{localtime()[5]}') #hour:mins:secs
+    print(f'{message} {localtime()[3]}:{localtime()[4]}:{localtime()[5]}') #hours:mins:secs
     print('\nAwaiting command...')
 
 def spy(ctx) -> Union[str, str]:
@@ -46,50 +46,45 @@ def spy(ctx) -> Union[str, str]:
     data = [str(id), str(name), str(mention)]
     data = ','.join(data)
     locale = ctx.guild.preferred_locale
-    users = [lines.strip() for lines in open(USERS)] #reads the file so that we don't have copys of users
+    users = [lines.strip() for lines in open(USERS)] #Reads the file so that we don't have copys of users.
     for line in users: 
-        #searches in all of lines from the csv
-        #then searches in all rows from the line
-        #if the id has a match it means that the user is already stored
-        for column in line.split(','):
-        #another way of doing this is `if line.split(',')[0] == str(id):`
-            if str(id) == column: #the user id is unique so we just verify if it's already in the csv
-                return mention, locale
+        #Searches in all of lines from the csv
+        #then looks at the first info on the line that is the id
+        #if the unique id has a match it means the user is already stored.
+        if line.split(',')[0] == str(id):
+            return mention, locale
     else:
-        open(USERS, 'a').write(f'{data}\n') #if it isn't we save it
+        open(USERS, 'a').write(f'{data}\n') #If the id is new we save it.
         status('New user registered')
         return mention, locale
 
-async def activity_changer() -> None: #this is the "status" under the bot name
+async def activity_changer() -> None: #This is the "status" under the bot name.
     """Changes the bot activity
     """ 
-    #name = f'{PREFIXES}help' #if you want to use a fix activity
-    name = choice(open(ACTIVITIES, 'r', encoding='UTF-8').readlines()) #randomly chooses one of the activities in the activities folder
-    activity = discord.Activity(name=name, type=discord.ActivityType.watching) #transforms it into the discord class
-    #the discord.ActivityType can be other things other than "watching", like: playing, streaming, listening
-    await client.change_presence(activity=activity) #makes the change
+    name = choice(open(ACTIVITIES, 'r', encoding='UTF-8').readlines()) #Randomly chooses one of the activities in the activities file.
+    activity = discord.Activity(name=name, type=discord.ActivityType.watching) #Transforms it into the discord class.
+    await client.change_presence(activity=activity)
 
-@client.event #decorator that gives a flag on event
-async def on_ready() -> None: #when the bot is ready for using do:
+@client.event #Decorator that gives a flag on event.
+async def on_ready() -> None: #When the bot is ready for using.
     """Notifies when it's ready and turn on schedulers"""
     status(f'\n{client.user.name} is on')
-    await activity_changer() #changes the bot's activity/status
-    #schedulers on ready
-    scheduler.add_job(activity_changer, CronTrigger(minute=30, second=0)) #creates a scheduled task, this one changes activity every 30 mins
-    scheduler.start() #starts all tasks under the scheduler object
+    await activity_changer()
+    scheduler.add_job(activity_changer, CronTrigger(minute=30, second=0)) #Creates a scheduled task, this one changes activity every 30 mins.
+    scheduler.start()
     status('Schedulers started')
 
-@client.command(hidden=True) #this is a Discord command, won't show on the help command
-@commands.has_permissions(administrator=True) #checks if the user that called it has the permission administrator
-async def reload(ctx, extension) -> None: #ctx (context) parameter gets the message object
+@client.command(hidden=True) #This is a Discord command, won't show on the help command.
+@commands.has_permissions(administrator=True) #Checks if the user that called it has the permission administrator.
+async def reload(ctx, extension) -> None:
     """Reloads a cog"""
     spy(ctx)
     client.reload_extension(f'cogs.{extension}')
     status(f'Reloading {extension}...')
-    await ctx.send(f'{extension} reloaded') #the bot sends a message to the same text channel it was called
+    await ctx.send(f'{extension} reloaded') #Bot sends a message to the same text channel it was called.
 
 @client.command(hidden=True)
-@commands.has_permissions(administrator=True) #if you don't want to limit who has access to the command just remove the line
+@commands.has_permissions(administrator=True)
 async def load(ctx, extension) -> None:
     """Loads an unloaded cog"""
     spy(ctx)
@@ -107,54 +102,55 @@ async def unload(ctx, extension) -> None:
     await ctx.send(f'{extension} unloaded')
 
 @client.command(aliases=['clear'])
-@commands.has_permissions(manage_messages=True) #the bot needs this permission as well, I don't recommend removing this check, because anyone could just clear all the server messages
+@commands.has_permissions(manage_messages=True) #Bot needs this server permission, I don't recommend removing this check.
 async def purge(ctx, amt=100) -> None:
     """Clears channel of an amount (default 100) of messages, needs manage messages permission"""
     spy(ctx)
-    await ctx.channel.purge(limit = amt + 1) #this makes the bot clear messages
+    await ctx.channel.purge(limit = amt + 1) #limit is the amount of messages the bot will clear + 1 for the command.
     await ctx.send(f'The last {amt} messages were purged')
     status(f'Purging {amt}...')
 
 @client.command()
 @commands.has_permissions(administrator=True)
-async def subscribe(ctx, act: str) -> None: #the act (activity) parameter is the name of csv file containing info from the channel
+async def subscribe(ctx, act: str) -> None:
     """Subscribes the channel to notifications, needs admin permission"""
     spy(ctx)
-    channel = str(ctx.channel.id) #gets the id of the channel where the command was used
-    name = str(ctx.channel.name) #gets the name of the channel where the command was used
-    guild = str(ctx.channel.guild) #gets the guild name from the channel where the command was used
-    data = [channel, name, guild]
+    id = str(ctx.channel.id)
+    name = str(ctx.channel.name)
+    guild = str(ctx.channel.guild)
+    data = [id, name, guild]
     data = ','.join(data)
     try:
         file = open(f'{INFO}/{act}.csv')
         subs = [lines.strip() for lines in file]
         for line in subs:
-            for row in line.split(','):
-                if channel == row: #if channel is already subscribed in the told activity
-                    await ctx.send('Channel already subscribed')
-                    return status('Oops')
-        else: #if it's not, it will save it
+            #Searches in all of lines from the csv
+            #then looks at the first info on the line that is the id
+            #if the unique id has a match it means the channel is already stored.
+            if line.split(',')[0] == id:
+                await ctx.send('Channel already subscribed')
+                return status('Oops')
+        else: #If the id is new we save it.
             open(f'{INFO}/{act}.csv', 'a', encoding='UTF-8').write(f'{data}\n')
             status(f'Subscribing {name} to {act}, from {guild}')
             await ctx.send(f'Channel {name} subscribed to {act} successfully')
-    except FileNotFoundError: #if the activity does not exist creates a new csv with it
+    except FileNotFoundError: #If the act doesn't exist creates a new csv named by it with the channel.
         file = open(f'{INFO}/{act}.csv', 'w', encoding='UTF-8').write(f'id,name,guild\n{data}\n')
         status(f'Subscribing {name} to {act}, from {guild}')
         await ctx.send(f'Channel {name} subscribed to {act} successfully')
 
 @client.command()
 @commands.has_permissions(administrator=True)
-async def unsubscribe(ctx, act: str) -> None: #reverse from subscribe ;)
+async def unsubscribe(ctx, act: str) -> None:
     """Unsubs the channel from notifications, needs admin permission"""
     spy(ctx)
-    channel = str(ctx.channel.id)
+    id = str(ctx.channel.id)
     try:
         file = open(f'{INFO}/{act}.csv', 'r')
         subs = [lines.strip() for lines in file]
         for line in subs:
-            for row in line.split(','):
-                if channel == row:
-                    del(line)
+            if line.split(',')[0] == id:
+                del(line)
         file = open(f'{INFO}/{act}.csv', 'w')
         for line in subs:
             file.write(f'{line}\n')
@@ -162,26 +158,29 @@ async def unsubscribe(ctx, act: str) -> None: #reverse from subscribe ;)
     except FileNotFoundError:
         await ctx.send(f'{act} does not exist')
 
-if __name__ == '__main__': #the bot only runs if this is the file being run, makes it cannot be run through imports
+if __name__ == '__main__': #Makes it can't be run through imports.
 
-    os.system('cls||clear') #clear the console for easier visualization
+    os.system('cls||clear') #Clear the console for easier visualization.
 
     print('='*50)
-    print(f'Welcome to {BOT} bot Ver. {VERSION}') #shows this info on console before booting, not necessary to funcition
+    print(f'Welcome to {BOT} bot Ver. {VERSION}')
     print('='*50)
-    sleep(0.5) #waits for half a second, not necessary to function
+    sleep(0.5)
 
     os.system('cls||clear')
 
-    for file in os.listdir(COGS): #this loads the cogs/extensions
+    for file in os.listdir(COGS):
+        #It looks for every file in the cogs folder
+        #then checks if it ends with py, if it does loads it
+        #if the file starts with # or is the core module it's no implemented.
         if file.endswith('.py'):
-            if file.startswith('core') or file.startswith('#'): #core is not a cog and files with # are no implemented
-                continue                                        #the choice for # is arbitrary, can be anything
+            if file.startswith('core') or file.startswith('#'): # # was arbritary
+                continue
             else:
-                client.load_extension(f'cogs.{file[:-3]}') #loads the extensions on "cogs" folder
-                print(f'Loading {file[:-3]} extension...') #prints on the console which extension was loaded
-                sleep(0.25) #waits for quarter of a second, not necessary to function
+                client.load_extension(f'cogs.{file[:-3]}')
+                print(f'Loading {file[:-3]} extension...')
+                sleep(0.25)
 
     os.system('cls||clear')
 
-    client.run(TOKEN) #runs the bot
+    client.run(TOKEN) #Runs the bot.
